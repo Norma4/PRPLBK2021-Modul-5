d3919e0f134467bfabdbae65eea7360c441bc460
Muhamad Irham Maulana <irhammaulana1077@gmail.com>
2021-09-24 14:51:09 +0700

    Update index.js

--- tugas_modul5_kel42/src/index.js
+++ tugas_modul5_kel42/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Tugas from './tugas';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Tugas />
   </React.StrictMode>,
   document.getElementById('root')
 );
